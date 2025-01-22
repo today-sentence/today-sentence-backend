0ecaeba2ac0df23ea9c6e7532d19f2b935129ce6
용훈 <yhn515@naver.com>
2025-01-22 16:50:27 +0900

    Feat: swagger-ui 설정 추가

---
+++ src/main/java/today/todaysentence/domain/user/api/UserController.java
@@ -0,0 +1,18 @@
+package today.todaysentence.domain.user.api;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Tag(name = "유저")
+@RestController("/users")
+public class UserController {
+
+    @Operation(summary = "회원 가입")
+    @PostMapping("/join")
+    public ResponseEntity<?> join() {
+        return ResponseEntity.ok().build();
+    }
+}
