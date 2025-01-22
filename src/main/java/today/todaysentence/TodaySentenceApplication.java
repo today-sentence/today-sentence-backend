822dd92643ba9183d019cd6826a92b155e204809
hoooonshub <yhn515@naver.com>
2025-01-22 10:49:10 +0900

    Init: 스프링부트 초기화

---
+++ src/main/java/today/todaysentence/TodaySentenceApplication.java
@@ -0,0 +1,13 @@
+package today.todaysentence;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodaySentenceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TodaySentenceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/today/todaysentence/TodaySentenceApplicationTests.java
@@ -0,0 +1,13 @@
+package today.todaysentence;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodaySentenceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
