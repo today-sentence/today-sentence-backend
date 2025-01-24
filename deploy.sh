71109300b8eb1210449eb1a73c5302df7c8eae46
용훈 <yhn515@naver.com>
2025-01-24 14:00:26 +0900

    Feat: dev 브랜치 CI/CD 파이프라인 구축

--- deploy.sh
+++ deploy.sh
@@ -1,47 +1,37 @@
 #!/bin/bash
 
 # 사용자 입력
-KEY_PATH="/c/today-sentence/mykey.pem"  # EC2 접속에 사용할 .pem 키 파일 경로
-EC2_USER="ubuntu"                      # EC2 사용자 이름
-EC2_IP="ec2-13-209-47-32.ap-northeast-2.compute.amazonaws.com"  # EC2 퍼블릭 DNS
-JAR_FILE="/c/today-sentence/today-sentence-backend/build/libs/today-sentence-0.0.1-SNAPSHOT.jar"  # JAR 파일 경로 수정
+
+JAR_FILE=$1   # JAR 파일 이름
+EC2_USER=$2                      # EC2 사용자 이름
+EC2_IP=$3    # EC2 퍼블릭 DNS
+KEY_PEM=$4
 REMOTE_PATH="/home/ubuntu"  # EC2에서 파일을 저장할 경로
-APP_NAME="today-sentence-0.0.1-SNAPSHOT.jar"  # EC2에 저장될 파일 이름
+APP_NAME=$JAR_FILE  # EC2에 저장될 파일 이름
 
-# 1. JAR 파일 업로드
-echo "Uploading $JAR_FILE to $EC2_USER@$EC2_IP:$REMOTE_PATH"
-scp -i "$KEY_PATH" "$JAR_FILE" "$EC2_USER@$EC2_IP:$REMOTE_PATH/$APP_NAME"
-if [ $? -ne 0 ]; then
-  echo "File upload failed. Please check the JAR file path or EC2 connection."
-  exit 1
-fi
+# 1. 수행 중인 애플리케이션 pid 확인
+CURRENT_PID=${pgrep -f $JAR_FILE}
 
-# 2. EC2 접속 및 애플리케이션 종료
-echo "Connecting to EC2 and stopping existing application..."
-ssh -i "$KEY_PATH" "$EC2_USER@$EC2_IP" << EOF
-  pkill -f "$APP_NAME" || echo "No running application to stop."
-EOF
-if [ $? -ne 0 ]; then
-  echo "Failed to stop existing application. Please check the EC2 connection."
-  exit 1
+echo "현재 실행 중인 $JAR_FILE 의 pid : $CURRENT_PID "
+
+
+# 2. 애플리케이션 종료
+if [ -z "$CURRENT_PID" ]; then
+        echo "> 현재 구동 중인 애플리케이션이 없으므로 종료하지 않습니다."
+else
+        echo "> kill -15 $CURRENT_PID"
+        kill -15 $CURRENT_PID
+        sleep 5
 fi
 
 # 3. 새 애플리케이션 실행
 echo "Starting new application..."
-ssh -i "$KEY_PATH" "$EC2_USER@$EC2_IP" << EOF
-  nohup java -jar "$REMOTE_PATH/$APP_NAME" > "$REMOTE_PATH/app.log" 2>&1 &
-EOF
+nohup java -jar "$REMOTE_PATH/$JAR_FILE" > "$REMOTE_PATH/app.log" 2>&1 &
+
 if [ $? -ne 0 ]; then
   echo "Failed to start the new application. Please check the JAR file or Java environment on EC2."
   exit 1
 fi
 
-# 4. 로그 출력
-echo "Deployment completed. Showing logs:"
-ssh -i "$KEY_PATH" "$EC2_USER@$EC2_IP" "tail -n 20 $REMOTE_PATH/app.log"
-if [ $? -ne 0 ]; then
-  echo "Failed to retrieve logs. Please check the EC2 connection or log file path."
-  exit 1
-fi
-
-# 5.끝
+# 끝
+echo "Deployment finished successfully."
