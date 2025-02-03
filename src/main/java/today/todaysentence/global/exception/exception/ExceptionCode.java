70c86f058f341f9e03dda188fe06ca2b824b60aa
KimHyunJun-55 <mical154@naver.com>
2025-02-03 20:19:19 +0900

    feat : MemberService CustomException add

--- src/main/java/today/todaysentence/global/exception/exception/ExceptionCode.java
+++ src/main/java/today/todaysentence/global/exception/exception/ExceptionCode.java
@@ -1,25 +1,33 @@
 package today.todaysentence.global.exception.exception;
 
-import static org.springframework.http.HttpStatus.BAD_REQUEST;
-import static org.springframework.http.HttpStatus.NOT_FOUND;
-
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+
+import static org.springframework.http.HttpStatus.*;
 
 @Getter
 @RequiredArgsConstructor
 public enum ExceptionCode {
 
-    // user
+    // member
     INVALID_EMAIL_FORMAT("이메일의 형식이 올바르지 않습니다.", BAD_REQUEST),
     PASSWORD_ERROR("비밀번호를 확인해주세요.", BAD_REQUEST),
+    MEMBER_NOT_FOUND("사용자를 찾을 수 없습니다.", NOT_FOUND),
+    DUPLICATED_EMAIL("이미 사용중인 이메일 입니다.", BAD_REQUEST),
+    DUPLICATED_NICKNAME("이미 사용중인 닉네임 입니다.", BAD_REQUEST),
 
     // post
     POST_NOT_FOUND("게시된 명언 글을 찾을 수 없습니다.", NOT_FOUND),
 
     //
     PARAMETER_VALIDATION_FAIL("파라미터가 올바르지 않습니다.", BAD_REQUEST),
+
+    INVALID_TOKEN( "유효하지 않은 토큰입니다.",UNAUTHORIZED),
+    EXPIRED_TOKEN( "만료된 토큰입니다.",UNAUTHORIZED),
+    UNSUPPORTED_TOKEN( "지원되지 않는 토큰입니다.",UNAUTHORIZED),
+    WRONG_TOKEN(  "잘못된 토큰입니다.",UNAUTHORIZED),
+    NOT_ACCORDANCE_DEVICE_ID("기존접속과 정보가 다릅니다.",BAD_REQUEST)
     ;
 
     private final String message;
