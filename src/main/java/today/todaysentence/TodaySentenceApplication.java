cc04d3e067134f2a3a6f049ec75686bdb77a690c
KimHyunJun-55 <mical154@naver.com>
2025-02-03 20:18:31 +0900

    feat : annotation add for aop

--- src/main/java/today/todaysentence/TodaySentenceApplication.java
+++ src/main/java/today/todaysentence/TodaySentenceApplication.java
@@ -2,10 +2,12 @@ package today.todaysentence;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableScheduling
 @SpringBootApplication
+@EnableAspectJAutoProxy
 public class TodaySentenceApplication {
 
     public static void main(String[] args) {
