c7a0dec77a7bb59bb2b1c5960cd7c229db29a3e4
hoooonshub <yhn515@naver.com>
2025-02-04 17:35:01 +0900

    Fix: User 테이블명 -> Users 변경

--- src/main/java/today/todaysentence/domain/user/User.java
+++ src/main/java/today/todaysentence/domain/user/User.java
@@ -4,11 +4,22 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
 
+@Table(name = "USERS")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Entity
 public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    private String email;
+
+    public User(String email) {
+        this.email = email;
+    }
 }
