2685cd8ba9424ab01569cd5624257fd7a240afba
hoooonshub <yhn515@naver.com>
2025-02-04 17:29:13 +0900

    Fix: 예외 응답 형태 수정

--- src/main/java/today/todaysentence/global/exception/advice/ErrorResponse.java
+++ src/main/java/today/todaysentence/global/exception/advice/ErrorResponse.java
@@ -2,9 +2,16 @@ package today.todaysentence.global.exception.advice;
 
 import static today.todaysentence.global.exception.exception.ExceptionCode.PARAMETER_VALIDATION_FAIL;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 import today.todaysentence.global.exception.exception.BaseException;
 
-public record ErrorResponse(String message) {
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ErrorResponse {
+    private String message;
 
     static ErrorResponse from(BaseException exception) {
         return new ErrorResponse(exception.getMessage());
