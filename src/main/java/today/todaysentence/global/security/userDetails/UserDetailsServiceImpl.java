7e1ff3d35e52f94779f78d319648171b55f40a24
KimHyunJun-55 <mical154@naver.com>
2025-02-05 21:26:38 +0900

    chore : findByCustomEmail > findByEmail naming edit

--- src/main/java/today/todaysentence/global/security/userDetails/UserDetailsServiceImpl.java
+++ src/main/java/today/todaysentence/global/security/userDetails/UserDetailsServiceImpl.java
@@ -19,7 +19,7 @@ public class UserDetailsServiceImpl implements UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
-        return memberRepository.findByCustomEmail(email)
+        return memberRepository.findByEmail(email)
                 .map(CustomUserDetails::new)
                 .orElseThrow(() -> new BaseException(ExceptionCode.MEMBER_NOT_FOUND));
 
