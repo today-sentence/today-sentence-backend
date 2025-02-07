af6a7f566bd03093f8d82a0211fa768bef4d2966
KimHyunJun-55 <mical154@naver.com>
2025-02-07 22:50:19 +0900

    feat : validAnnotation(message, nickname)

---
+++ src/main/java/today/todaysentence/global/annotation/ValidMessage.java
@@ -0,0 +1,37 @@
+package today.todaysentence.global.annotation;
+
+
+import jakarta.validation.Constraint;
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+import jakarta.validation.Payload;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.FIELD)
+@Retention(RetentionPolicy.RUNTIME)
+@Constraint(validatedBy = ValidMessage.MessageValidator.class)
+public @interface ValidMessage {
+
+    //정규식 바뀔 수 있음.
+    String message() default "상태메시지는 1자이상 50자 이하여야합니다.";
+
+    Class<?>[] groups() default {};
+
+    Class<? extends Payload>[] payload() default {};
+
+    class MessageValidator implements ConstraintValidator<ValidMessage, String> {
+
+        @Override
+        public boolean isValid(String message, ConstraintValidatorContext context) {
+            if (message == null ) {
+                return false;
+            }
+
+            return message.matches(".{1,50}");
+        }
+    }
+}
---
+++ src/main/java/today/todaysentence/global/annotation/ValidNickname.java
@@ -0,0 +1,37 @@
+package today.todaysentence.global.annotation;
+
+
+import jakarta.validation.Constraint;
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+import jakarta.validation.Payload;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.FIELD)
+@Retention(RetentionPolicy.RUNTIME)
+@Constraint(validatedBy = ValidNickname.NicknameValidator.class)
+public @interface ValidNickname {
+
+    //정규식 바뀔 수 있음.
+    String message() default "닉네임은  한글, 특수문자, 영문 으로 가능하며 1자이상 8자 이하여야합니다.";
+
+    Class<?>[] groups() default {};
+
+    Class<? extends Payload>[] payload() default {};
+
+    class NicknameValidator implements ConstraintValidator<ValidNickname, String> {
+
+        @Override
+        public boolean isValid(String message, ConstraintValidatorContext context) {
+            if (message == null) {
+                return false;
+            }
+
+            return message.matches("^[a-zA-Z0-9가-힣!@#$%^&*()_+=-]{1,8}$");
+        }
+    }
+}
