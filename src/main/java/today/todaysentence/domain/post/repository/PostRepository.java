e321ed15c037e7d726b2890517a681ae01f2aedb
KimHyunJun-55 <mical154@naver.com>
2025-02-09 18:23:12 +0900

    feat : posts search

--- src/main/java/today/todaysentence/domain/post/repository/PostRepository.java
+++ src/main/java/today/todaysentence/domain/post/repository/PostRepository.java
@@ -1,11 +1,14 @@
 package today.todaysentence.domain.post.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.lang.NonNull;
 import today.todaysentence.domain.member.Member;
 import today.todaysentence.domain.post.Post;
+import today.todaysentence.domain.search.dto.SearchResponse;
 
 import java.util.List;
 import java.util.Optional;
@@ -18,4 +21,91 @@ public interface PostRepository extends JpaRepository<Post, Long> {
                                  @Param("year") int year);
 
     Optional<Post> findById(@NonNull Long id);
+
+
+    @Query(value = "SELECT " +
+            "b.title , b.author , b.cover, b.publisher, b.publishing_year, " +
+            "p.id , m.nickname, p.content, p.category , " +
+            "GROUP_CONCAT(DISTINCT h.name), " +
+            "COUNT(l.id) as like_count " +
+            "FROM post p " +
+            "INNER JOIN member m ON m.id = p.writer_id " +
+            "INNER JOIN book b ON b.isbn = p.book_isbn " +
+            "INNER JOIN post_hashtag ph ON ph.post_id = p.id " +
+            "LEFT JOIN likes l ON l.post_id = p.id " +
+            "LEFT JOIN hashtag h ON h.id = ph.hashtag_id " +
+            "WHERE b.title = :search " +
+            "GROUP BY p.id " +
+            "ORDER BY like_count DESC " +
+            "LIMIT :limit OFFSET :offset", nativeQuery = true)
+    List<SearchResponse.PostSearchResult> findPostsByBookTitle(
+            @Param("search") String search,
+            @Param("limit") int limit,
+            @Param("offset") int offset);
+
+
+    @Query(value = "SELECT " +
+            "b.title, b.author, b.cover, b.publisher, b.publishing_year, " +
+            "p.id, m.nickname, p.content, p.category, " +
+            "GROUP_CONCAT(DISTINCT h.name) AS hashtags, " +
+            "COUNT(l.id) AS like_count " +
+            "FROM post p " +
+            "INNER JOIN member m ON m.id = p.writer_id " +
+            "INNER JOIN book b ON b.isbn = p.book_isbn " +
+            "INNER JOIN post_hashtag ph ON ph.post_id = p.id " +
+            "LEFT JOIN likes l ON l.post_id = p.id " +
+            "LEFT JOIN hashtag h ON h.id = ph.hashtag_id " +
+            "WHERE ph.post_id IN (" +
+            "  SELECT ph.post_id " +
+            "  FROM post_hashtag ph " +
+            "  INNER JOIN hashtag h ON h.id = ph.hashtag_id " +
+            "  WHERE h.name LIKE :search" +
+            ") " +
+            "GROUP BY p.id " +
+            "ORDER BY like_count DESC " +
+            "LIMIT :limit OFFSET :offset", nativeQuery = true)
+    List<SearchResponse.PostSearchResult> findPostsHashtag(@Param("search") String search,
+                                                           @Param("limit") int limit,
+                                                           @Param("offset") int offset);
+
+    @Query(value = "SELECT " +
+            "b.title , b.author , b.cover, b.publisher, b.publishing_year, " +
+            "p.id , m.nickname, p.content, p.category , " +
+            "GROUP_CONCAT(DISTINCT h.name), " +
+            "COUNT(l.id) as like_count " +
+            "FROM post p " +
+            "INNER JOIN member m ON m.id = p.writer_id " +
+            "INNER JOIN book b ON b.isbn = p.book_isbn " +
+            "INNER JOIN post_hashtag ph ON ph.post_id = p.id " +
+            "LEFT JOIN likes l ON l.post_id = p.id " +
+            "LEFT JOIN hashtag h ON h.id = ph.hashtag_id " +
+            "WHERE p.category = :search " +
+            "GROUP BY p.id " +
+            "ORDER BY like_count DESC " +
+            "LIMIT :limit OFFSET :offset", nativeQuery = true)
+    List<SearchResponse.PostSearchResult> findPostsCategory(@Param("search") String search,
+                                                           @Param("limit") int limit,
+                                                           @Param("offset") int offset);
+
+//    검색결과의 총 개수 혹시나 추후사용 가능성이 있어서 만들어만놓았어요
+//    @Query(value = "SELECT COUNT(DISTINCT p.id) " +
+//            "FROM post p " +
+//            "INNER JOIN member m ON m.id = p.writer_id " +
+//            "INNER JOIN book b ON b.isbn = p.book_isbn " +
+//            "INNER JOIN post_hashtag ph ON ph.post_id = p.id " +
+//            "LEFT JOIN likes l ON l.post_id = p.id " +
+//            "LEFT JOIN hashtag h ON h.id = ph.hashtag_id " +
+//            "WHERE b.title = :search",
+//             nativeQuery = true)
+//    Long countPostsByCategory(@Param("search") String search);
 }
+
+
+
+
+
+
+
+
+
+
--- src/main/java/today/todaysentence/domain/search/service/SearchService.java
+++ src/main/java/today/todaysentence/domain/search/service/SearchService.java
@@ -5,17 +5,20 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import today.todaysentence.domain.book.repository.BookRepository;
+import today.todaysentence.domain.post.repository.PostRepository;
 import today.todaysentence.domain.search.dto.SearchResponse;
 import today.todaysentence.global.exception.exception.BaseException;
 import today.todaysentence.global.exception.exception.ExceptionCode;
 import today.todaysentence.global.response.CommonResponse;
 
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class SearchService {
 
     private final BookRepository bookRepository;
+    private final PostRepository postRepository;
 
     public CommonResponse<?> findBooks(String type, String search, Pageable pageable) {
 
@@ -34,6 +37,30 @@ public class SearchService {
 
 
         return CommonResponse.ok(books);
+    }
+
+    public CommonResponse<?> findPosts(String type, String search , Pageable pageable) {
+
+        int limit = pageable.getPageSize();
+        int offset = pageable.getPageNumber()*pageable.getPageSize();
+
+        List<SearchResponse.PostSearchResult> posts;
+
+        if ("title".equals(type)) {
+            posts = postRepository.findPostsByBookTitle(search,limit,offset);
+        } else if ("tag".equals(type)) {
+           posts = postRepository.findPostsHashtag(search,limit,offset);
+        }else if ("category".equals(type)) {
+           posts = postRepository.findPostsCategory(search,limit,offset);
+        }else{
+            throw new BaseException(ExceptionCode.NOT_MATCHED_TYPE_PARAMETER);
+        }
+        if (posts.isEmpty()) {
+            return CommonResponse.ok("검색 결과가 없습니다.");
+        }
+
+
+        return CommonResponse.ok(posts);
     }
 
 
