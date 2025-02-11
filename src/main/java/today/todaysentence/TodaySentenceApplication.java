4638fdc918838ba73150d475faaccdd93a21b400
KimHyunJun-55 <mical154@naver.com>
2025-02-11 22:40:14 +0900

    feat: set server time zone to Asia

--- src/main/java/today/todaysentence/TodaySentenceApplication.java
+++ src/main/java/today/todaysentence/TodaySentenceApplication.java
@@ -1,10 +1,13 @@
 package today.todaysentence;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableScheduling;
+
+import java.util.TimeZone;
 
 @EnableScheduling
 @SpringBootApplication
@@ -14,6 +17,11 @@ public class TodaySentenceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(TodaySentenceApplication.class, args);
+    }
+
+    @PostConstruct
+    public void setTimeZone(){
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
     }
 
 }
