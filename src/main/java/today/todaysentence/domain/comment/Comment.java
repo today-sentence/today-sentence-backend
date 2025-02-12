536defac1243a19d2ac128ed81099100f9b8fc5a
hoooonshub <yhn515@naver.com>
2025-02-12 15:06:16 +0900

    Feat(.comment): post_id와 createAt 복합 인덱스 추가

--- src/main/java/today/todaysentence/domain/comment/Comment.java
+++ src/main/java/today/todaysentence/domain/comment/Comment.java
@@ -5,14 +5,17 @@ import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Index;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import today.todaysentence.domain.member.Member;
 import today.todaysentence.global.timeStamped.Timestamped;
 
+@Table(indexes = @Index(name = "idx_comment_post_id_create_at", columnList = "postId, createAt"))
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Entity
