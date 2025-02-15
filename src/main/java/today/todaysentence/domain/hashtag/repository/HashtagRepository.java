0f2bda6f0c20a3731f99cc8aef3ff1189d85d5ad
KimHyunJun-55 <mical154@naver.com>
2025-02-15 21:49:41 +0900

    feat : associatedSearch demo

--- src/main/java/today/todaysentence/domain/hashtag/repository/HashtagRepository.java
+++ src/main/java/today/todaysentence/domain/hashtag/repository/HashtagRepository.java
@@ -1,11 +1,16 @@
 package today.todaysentence.domain.hashtag.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import today.todaysentence.domain.hashtag.Hashtag;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface HashtagRepository extends JpaRepository<Hashtag, Long> {
 
     Optional<Hashtag> findByName(String name);
+
+    @Query("SELECT h FROM Hashtag h")
+    List<Hashtag> findAllName();
 }
