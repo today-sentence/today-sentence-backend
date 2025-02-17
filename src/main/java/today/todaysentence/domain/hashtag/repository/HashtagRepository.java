8765ac54b5aa834b322e41d07e18f8c839a69b6c
KimHyunJun-55 <mical154@naver.com>
2025-02-17 20:09:19 +0900

    feat: update hashtags using schedule

--- src/main/java/today/todaysentence/domain/hashtag/repository/HashtagRepository.java
+++ src/main/java/today/todaysentence/domain/hashtag/repository/HashtagRepository.java
@@ -2,6 +2,7 @@ package today.todaysentence.domain.hashtag.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import today.todaysentence.domain.hashtag.Hashtag;
 
 import java.util.List;
@@ -13,4 +14,13 @@ public interface HashtagRepository extends JpaRepository<Hashtag, Long> {
 
     @Query("SELECT h FROM Hashtag h")
     List<Hashtag> findAllName();
+
+
+    @Query("SELECT h FROM Hashtag h")
+    List<Hashtag> findAllIds();
+
+    @Query("SELECT h FROM Hashtag h WHERE h.id NOT IN :hashIds")
+    List<Hashtag> findNewIds(@Param("hashIds") List<Long> hashIdsLong);
+
+
 }
