2726ea4d1d08fc43e7e240b826b66a3d000f53e9
KimHyunJun-55 <mical154@naver.com>
2025-02-24 13:47:08 +0900

    refactor: move searchResult from client-side to server-side

--- src/main/java/today/todaysentence/domain/search/service/SearchService.java
+++ src/main/java/today/todaysentence/domain/search/service/SearchService.java
@@ -61,30 +61,43 @@ public class SearchService {
     }
 
     @Transactional(readOnly = true)
-    public CommonResponse<?> findPosts(String type, String search, JwtUserDetails userDetails) {
+    public CommonResponse<?> findPosts(
+            String type,
+            String search,
+            String sortBy,
+            int size,
+            int page,
+            JwtUserDetails userDetails
+    ) {
 
         List<PostResponseDTO> posts;
 
-        String query;
-        if ("title".equals(type)) {
-            query="b.title = '" + search + "'";
-            posts = postRepositoryCustom.findPostsByDynamicQuery(query);
+        String query = null;
+        String orderBy = " like_count DESC ";
 
+        if(sortBy.equals("create_At")){
+            orderBy = " p.create_at DESC ";
+        }else if(sortBy.equals("like_count")){
+            orderBy = " like_count DESC ";
+        }
+        if ("title".equals(type)) {
+            query="b.title = :search";
         }else if ("category".equals(type)) {
-            query="p.category = '" + search + "'";
-            posts = postRepositoryCustom.findPostsByDynamicQuery(query);
+            query="p.category = :search";
         } else if ("tag".equals(type)) {
             redisService.saveOrUpdateKeyword("search",search);
             query= "ph.post_id IN (" +
                     "  SELECT ph.post_id " +
                     "  FROM post_hashtag ph " +
                     "  INNER JOIN hashtag h ON h.id = ph.hashtag_id " +
-                    "  WHERE h.name = '" +search +"'"+
+                    "  WHERE h.name = :search" +
                     ") ";
-                    posts = postRepositoryCustom.findPostsByDynamicQuery(query);
         }else{
             throw new BaseException(ExceptionCode.NOT_MATCHED_TYPE_PARAMETER);
         }
+
+        posts = postRepositoryCustom.findPostsByDynamicQuery(search,query,orderBy,size,page);
+
         if (posts.isEmpty()) {
             return CommonResponse.ok("검색 결과가 없습니다.");
         }
@@ -93,7 +106,14 @@ public class SearchService {
                 .toList();
         List<InteractionResponseDTO> interactions = memberService.checkInteractions(postIds, userDetails.id());
 
-        return CommonResponse.ok(new PostResponse.PostResults(posts,interactions));
+        Long totalCount = postRepositoryCustom.totalCount(query,search);
+        System.out.println("totalCount"+totalCount);
+
+        int totalPage = (int)Math.ceil(totalCount /(double)size);
+        boolean hasNextPage = (page+1) <totalPage;
+
+
+        return CommonResponse.ok(new PostResponse.PostResults(posts,interactions,totalPage,hasNextPage));
     }
 
     /**
